055fcd45677345735beca08f70153402c99f05e9
doge <doge@gmail.com>
2025-02-20 21:49:47 +0800

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/favicon.ico"/>
     <title>Minecraft 服务器统计</title>
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
