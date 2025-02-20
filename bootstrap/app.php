1d3e4519dbc7d9c134d7c3f6d24bf1084483eb09
doge <doge@gmail.com>
2025-02-20 21:28:57 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,12 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withSchedule(function ($schedule) {
+    ->withSchedule(function (Schedule $schedule) {
         // 每秒运行一次日志处理
         $schedule->command('minecraft:process-logs')
                 ->everySecond()
-                ->runInBackground()
-                ->withoutOverlapping();
+                ->sendOutputTo(storage_path('logs/process.log'), true);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
